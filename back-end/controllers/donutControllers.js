2b87566ecb09f2d9886e66dbe6485fe5c832cf57
Luis Arques <luisarquescalero@gmail.com>
2024-02-08 20:30:35 +0100

    Funciona traer todos los donuts

--- back-end/controllers/donutControllers.js
+++ back-end/controllers/donutControllers.js
@@ -2,12 +2,123 @@ const donutsDB = require("../mocks/donutsDB")
 const donutModel = require("../models/DonutModel")
 
 
-// 
+// GET todos los donuts 
+const getAllDonuts = async (req, res) => {
+    try{
 
+        const allDonuts = await donutModel.find()
+        const resDonut = allDonuts.map(donut => {
+            return {
+                id: donut.id,
+                nombre: donut.nombre,
+                tipo: donut.tipo
+            }
+        })
+        console.log(resDonut)
+        res.status(200).json({
+            status: 'succeeded',
+            data: resDonut,
+            error: null
+        })
 
+    }catch(error){
+        res
+        .status(500)
+        .json({ status: "failed", data: null, error: error.message }); 
+    }
+}
+// Get donut by Id
+const getDonutById = async (req, res) => {
+    try{
+        const id = req.params.id
+        const donut = await donutModel.findById(id)
+        res.status(200).json({
+            status: 'succeeded',
+            data: digimon,
+            error: null
+        })
 
+    }catch(error){
+        res
+        .status(500)
+        .json({ status: "failed", data: null, error: error.message }); 
+    }
+}
+// Create Donut
+const createDonut = async (req,res) => {
+    try{
+        const donutnData = req.body
+        const newDonut = await donutModel({
+            nombre: donutnData.nombre,
+            tipo: donutnData.tipo,
+            precio: donutnData.precio
+        })
+        await newDonut.save()
+        console.log(newDonut)
+        res.status(200).json({
+            status: 'succeeded',
+            data: newDonut,
+            error: null
+        })
 
+    }catch(error){
+        res
+        .status(500)
+        .json({ status: "failed", data: null, error: error.message }); 
+    }
+}
+// Actualizar donut
+const updateDonut = async (req,res) => {
+    try{
+        const id = req.params.id
+        const {nombre, tipo, precio} = req.body
+        
+        const donutAaux = await donutModel.findById(id)
 
+        if(!donutAaux) return res.status(404).send('El donut no existe')
+        
+        if(nombre) {
+            donutAaux.nombre = nombre
+        }
+        if(tipo){
+            donutAaux.tipo = tipo
+        }
+        if(precio){
+            donutAaux.precio = precio
+        }
+
+        await donutAaux.save()
+
+        res.status(200).json({
+            status: 'succeeded',
+            data: null,
+            error: null
+        })
+
+    }catch(error){
+        res
+        .status(500)
+        .json({ status: "failed", data: null, error: error.message }); 
+    }
+}
+
+// Eliminar donut
+const deleteDonut = async (req,res) => {
+    try{
+        const id = req.params.id
+        await donutModel.findByIdAndDelete(id);
+        res.status(200).json({
+            status: 'succeeded',
+            data: null,
+            error: null
+        })
+
+    }catch(error){
+        res
+        .status(500)
+        .json({ status: "failed", data: null, error: error.message }); 
+    }
+}
 
 // Carga inicial de datos
 const loadData = async(req, res) => {
@@ -28,4 +139,4 @@ const loadData = async(req, res) => {
     }
 }
 
-module.exports = {loadData}
+module.exports = {loadData, getAllDonuts, createDonut, getDonutById, updateDonut, deleteDonut}
--- back-end/routes/donutRoutes.js
+++ back-end/routes/donutRoutes.js
@@ -1,13 +1,13 @@
-const {loadData} = require("../controllers/donutControllers")
+const {loadData, getAllDonuts, createDonut, getDonutById, updateDonut, deleteDonut} = require("../controllers/donutControllers")
 
 const donutRouter = require("express").Router()
 
 // donutRouter.get('/loadData', loadData)
-donutRouter.get('/', getDonuts)
-donutRouter.get('/:id', getDonutById)
+donutRouter.get('/', getAllDonuts)
 donutRouter.post('/', createDonut)
+donutRouter.get('/:id', getDonutById)
 donutRouter.put('/:id', updateDonut)
-donutRouter.delete('/:id', getDonuts)
+donutRouter.delete('/:id', deleteDonut)
 
 
 module.exports = donutRouter
